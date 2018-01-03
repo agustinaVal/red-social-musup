6db27b3cd0a46cab2a90cd8e990c4e3d68ebfb9d
carol-solivos <carol.solivos@gmail.com>
2018-01-03 09:17:39 -0400

    carrousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
- 
+$(document).ready(function(){
+	$('.carousel').carousel({
+		indicators:true,
+		dist:-50, 
+		padding: 100
+	});
+	autoplay()   
+	function autoplay() {
+		$('.carousel').carousel('next');
+		setTimeout(autoplay, 4500);
+
+	}
+});

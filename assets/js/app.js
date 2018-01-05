7e195eebdfbe68d1332ea03212b262622aca4513
carol-solivos <carol.solivos@gmail.com>
2018-01-04 22:30:08 -0400

    galerias de imagenes y data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
 	$('.carousel').carousel({
-		indicators:true,
 		dist:-50,
 		padding: 100
 	});
-    $('.modal').modal();
+  $('.modal').modal();
+  for (var i = 0; i < 6; i++) {
+    perfiles = data.perfil[i];
+    $('.img' + (i+1)).append('<img class="circle" data-type="restaurant" src="assets/img/perfiles/' + perfiles + '">');
+    
+  }
+
 
 });
 
@@ -34,7 +39,7 @@ pauseButton.addEventListener("click", function() {
   vid.classList.toggle("stopfade");
   if (vid.paused) {
     vid.play();
-    pauseButton.innerHTML = "<i class='fa fa-pause'></i>Pausar";
+    pauseButton.innerHTML = "<i class='fa fa-pause'></i>Pausar <img src='assets/img/play.gif'>";
   } else {
     vid.pause();
     pauseButton.innerHTML = "<i class='fa fa-play'></i>Reproducir";
---
+++ assets/js/data.js
@@ -0,0 +1,3 @@
+var data = {
+	'perfil': ['perfil1.jpg', 'perfil2.jpg', 'perfil4.png', 'perfil3.jpg', 'perfil5.jpg', 'perfil6.jpg']
+}

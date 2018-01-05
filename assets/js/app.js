f78970fe53dacc265acb24400452b5f189adede7
carol-solivos <carol.solivos@gmail.com>
2018-01-05 02:28:16 -0400

    menu login

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,22 @@ $(document).ready(function(){
   for (var i = 0; i < 6; i++) {
     perfiles = data.perfil[i];
     $('.img' + (i+1)).append('<img class="circle" data-type="restaurant" src="assets/img/perfiles/' + perfiles + '">');
-    
-  }
-
-
+  };
+  
+  $('#ingresar').click(function(){
+    var user = $('#usuario').val();
+    var clave = $('#clave').val();
+    $('.btnSignIn').hide();
+    $('#listHeader').append('<li class="agus"><a href="bio.html"><img src="assets/img/agus.jpg" alt="" class="circle"></a></li><li><a href="bio.html">' + user + '</a></li>');
+    $('.brand-logo').before('<a href="#" data-activates="slide-out" class="btnUser"><i class="material-icons">menu</i></a>');
+    $('#slide-out').show();
+    $(".btnUser").sideNav();
+  });
+  $('#send').click(function(){
+        var inputWrite = $('#inputWrite').val();
+        $('#printMessage').append('<div class="message">' + inputWrite + '</div>');
+        $('#inputWrite').val('');
+  });
 });
 
 var vid = document.getElementById("bgvid");
@@ -20,11 +32,11 @@ if (window.matchMedia('(prefers-reduced-motion)').matches) {
     vid.removeAttribute("autoplay");
     vid.pause();
     pauseButton.innerHTML = "Paused";
-}
+};
 
 function vidFade() {
   vid.classList.add("stopfade");
-}
+};
 
 vid.addEventListener('ended', function()
 {
@@ -44,10 +56,10 @@ pauseButton.addEventListener("click", function() {
     vid.pause();
     pauseButton.innerHTML = "<i class='fa fa-play'></i>Reproducir";
   }
-})
+});
 
 var vid = document.getElementById("bgvid");
-	vid.volume = 0.09;
+	vid.volume = 0.08;
 var vid = document.getElementById("bgvid");
 	vid.currentTime = 11;
 
